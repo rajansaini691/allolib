b1daa3bf62112a6fb97f91272230ff15f2b79d8d
sphere <sphere@spherez05.mat.ucsb.edu>
2018-08-08 11:39:55 -0700

    Use git version of openmpi

--- cmake/find_additional_dependencies.cmake
+++ cmake/find_additional_dependencies.cmake
@@ -22,11 +22,11 @@ option(USE_MPI "" ON)
   )
 
   set(MPI_ASSUME_NO_BUILTIN_MPI ON)
-  set (MPI_INSTALL_PREFIX "${al_path}/../openmpi-3.1.1/local")
+  set (MPI_INSTALL_PREFIX "${al_path}/../ompi/local")
   # find_library (MPI_CXX_LIBRARIES NAMES mpi libmpi PATHS "${MPI_INSTALL_PREFIX}/lib" NO_DEFAULT_PATH NO_CMAKE_SYSTEM_PATH NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_PATH)
   # # set (MPI_CXX_LIBRARIES "${MPI_INSTALL_PREFIX}/lib/libmpi.so")
   set (MPI_CXX_ADDITIONAL_INCLUDE_DIRS "${MPI_INSTALL_PREFIX}/include")
-  set (MPI_CXX_COMPILER "${MPI_INSTALL_PREFIX}/bin/mpicxx")
+  set (MPI_CXX_COMPILER "${MPI_INSTALL_PREFIX}/bin/ortecc")
 
   find_package(FreeImage QUIET)
   find_package(Assimp QUIET)
