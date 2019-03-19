e7349d18ea4c92ebb16fde6837277db850b223fa
Kon Hyong Kim <konhyong@gmail.com>
2019-03-19 12:31:56 -0700

    fixed conflicts

--- run.sh
+++ run.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-usage="$(basename "$0") [-h] [-d] [-n] [-c] [-v] source.cpp
+usage="Usage: $(basename "$0") [-h] [-d] [-n] [-c] [-v] source.cpp
 -- build and run single-file allolib applications.
 
-where:
-    -h  show this help text
-    -d build debug version and run in debugger
-    -n build only. Don't run.
-    -c clean build
-    -v verbose build. Prints full cmake log and verbose build.
+options:
+  -h show this help text
+  -d build debug version and run in debugger
+  -n build only. Don't run.
+  -c clean build
+  -v verbose build. Prints full cmake log and verbose build.
 "
 
-
-INITIALDIR=${PWD} # gives absolute path
-# echo "Script executed from: ${INITIALDIR}"
+APP_ROOT=${PWD} # gives absolute path
+# echo "Script executed from: ${APP_ROOT}"
 
 # BASH_SOURCE has the script's path
 # could be absolute, could be relative
@@ -24,7 +23,7 @@ if [ ${FIRSTCHAR} == "/" ]; then
   AL_LIB_PATH=${SCRIPT_PATH}
 else
   # SCRIPT_PATH was relative
-  AL_LIB_PATH=${INITIALDIR}/${SCRIPT_PATH}
+  AL_LIB_PATH=${APP_ROOT}/${SCRIPT_PATH}
 fi
 
 # Get the number of processors on OS X; Linux; or MSYS2, or take a best guess.
@@ -91,6 +90,7 @@ fi
     ;;
   esac
 done
+
 # consume options that were parsed
 shift $(expr $OPTIND - 1 )
 
@@ -109,9 +109,7 @@ fi
 APP_FILE_INPUT="$1" # first argument (assumming we consumed all the options above)
 APP_PATH=$(dirname ${APP_FILE_INPUT})
 APP_FILE=$(basename ${APP_FILE_INPUT})
-APP_NAME=${APP_FILE%.*} # remove extension (once, assuming .cpp)
-TARGET_NAME=$(basename ${APP_FILE_INPUT} | sed 's/\.[^.]*$//')
-
+APP_NAME=$(basename ${APP_FILE_INPUT} | sed 's/\.[^.]*$//') # remove extension
 
 # echo "app path: ${APP_PATH}"
 # echo "app file: ${APP_FILE}"
@@ -123,10 +121,9 @@ TARGET_NAME=$(basename ${APP_FILE_INPUT} | sed 's/\.[^.]*$//')
       echo "cleaning build"
     fi
     rm -r build
-    rm -r build${BUILD_DIR_SUFFIX}
   fi
-  mkdir -p build${BUILD_DIR_SUFFIX}
-  cd build${BUILD_DIR_SUFFIX}
+  mkdir -p build
+  cd build
   mkdir -p ${APP_NAME}
   cd ${APP_NAME}
   mkdir -p ${BUILD_TYPE}
@@ -134,18 +131,18 @@ TARGET_NAME=$(basename ${APP_FILE_INPUT} | sed 's/\.[^.]*$//')
 
   echo cmake ${GENERATOR} ${WINDOWS_FLAGS} -Wno-deprecated -DBUILD_EXAMPLES=0 -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DAL_APP_FILE=../../../${APP_PATH}/${APP_FILE} -DAL_VERBOSE_OUTPUT=${VERBOSE_FLAG} ${VERBOSE_MAKEFILE} -DAL_APP_RUN=${RUN_APP} ${AL_LIB_PATH}/cmake/single_file
 
-  cmake ${GENERATOR} ${WINDOWS_FLAGS} -Wno-deprecated -DBUILD_EXAMPLES=0 -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DAL_APP_FILE=../../../${APP_PATH}/${APP_FILE} -DAL_VERBOSE_OUTPUT=${VERBOSE_FLAG} ${VERBOSE_MAKEFILE} -DAL_APP_RUN=${RUN_APP} ${AL_LIB_PATH}/cmake/single_file > cmake_log.txt
+  cmake ${GENERATOR} ${WINDOWS_FLAGS} -Wno-deprecated -DBUILD_EXAMPLES=0 -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DAL_APP_ROOT=${APP_ROOT} -DAL_APP_FILE=${APP_ROOT}/${APP_PATH}/${APP_FILE} -DAL_VERBOSE_OUTPUT=${VERBOSE_FLAG} ${VERBOSE_MAKEFILE} -DAL_APP_RUN=${RUN_APP} ${AL_LIB_PATH}/cmake/single_file > cmake_log.txt
 
-if [ ${RUN_APP} == 1 ]; then
-  TARGET_NAME=${TARGET_NAME}_run  
-fi
-if [ ${BUILD_TYPE} == "Debug" ]; then
-  TARGET_NAME=${TARGET_NAME}_debug
-fi
+  if [ ${RUN_APP} == 1 ]; then
+    APP_NAME=${APP_NAME}_run  
+  fi
+  if [ ${BUILD_TYPE} == "Debug" ]; then
+    APP_NAME=${APP_NAME}_debug
+  fi
 
-echo cmake --build . --target ${TARGET_NAME} --config ${BUILD_TYPE} -- ${BUILD_FLAGS}
+  echo cmake --build . --target ${APP_NAME} --config ${BUILD_TYPE} -- ${BUILD_FLAGS}
 
-cmake --build . --target ${TARGET_NAME} --config ${BUILD_TYPE} -- ${BUILD_FLAGS}
+  cmake --build . --target ${APP_NAME} --config ${BUILD_TYPE} -- ${BUILD_FLAGS}
 )
 
 APP_BUILD_RESULT=$?
@@ -158,16 +155,16 @@ fi
 # run app
 # go to where the binary is so we have cwd there
 # (app's cmake is set to put binary in 'bin')
-cd ${INITIALDIR}
-cd ${APP_PATH}/bin
 
 if [ ${RUN_MPI}  != 0 ]; then
+  cd ${APP_ROOT}
+  cd ${APP_PATH}/bin
   echo Running MPI at: `pwd`
 
   mpirun --mca btl_tcp_if_include enp1s0 --hostfile ../../mpi_hosts.txt /usr/bin/env DISPLAY=:0 ./"${APP_NAME}${POSTFIX}"
-#elif [ ${BUILD_TYPE} == "Debug" ]; then
-#echo "Running in debugger ${DEBUGGER}"
-#${DEBUGGER} ./"${APP_NAME}${POSTFIX}"
-#else
-#./"${APP_NAME}${POSTFIX}"
+# elif [ ${BUILD_TYPE} == "Debug" ]; then
+#   echo "Running in debugger ${DEBUGGER}"
+#   ${DEBUGGER} ./"${APP_NAME}${POSTFIX}"
+# else
+#   ./"${APP_NAME}${POSTFIX}"
 fi
