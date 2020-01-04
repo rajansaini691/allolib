c683263672772b370482a20a2daa6ae35735e74e
Rajan <rajansaini691@gmail.com>
2020-01-03 16:41:40 -0800

    Improved docs

---
+++ src/scene/al_Scale.cpp
@@ -0,0 +1,8 @@
+@include "al/scene/al_ScaleManager.hpp"
+
+using namespace al;
+
+Scale(std::string sclPath, unsigned int pitchStandard = 440) {
+	this->pitchStandard = pitchStandard;
+	// TODO Read and parse the scala file
+}
