ca4acc219829f66c6a3b28adc5235afdd9231af4
Rajan <rajansaini691@gmail.com>
2020-01-06 18:08:58 -0800

    [WIP] scl file parsing

--- src/scene/al_Scale.cpp
+++ src/scene/al_Scale.cpp
@@ -9,6 +9,39 @@ Scale::Scale(std::string sclPath, unsigned int tonic = 440) {
 	// TODO If the below code has become too large/messy to be readable, abstract
 	// into own function
 
+	// Stores the user's description of the tuning system 
+	std::string description;
+
+	// Stores the number of lines
+	int numLines = 0;
+
+	// Stores each note's distance from the tonic in cents
+	float* distances;
+
+	std::ifstream sclfile("thefile.scl");
+	std::string line;
+	
+	// The number of non-commented lines seen
+	int count = 0;
+	while (std::getline(sclfile, line)) {
+		// Comments start with !
+		if(line.size() == 0 || line[0] == '!') continue;
+
+		if(count == 0) {
+			description = line;
+			std::cout << "Description: " << description << std::endl;
+		} else if(count == 1) {
+			numLines = std::stoi(line);
+			std::cout << "The file uses " << numLines << " lines\n";
+			scalaAssert(
+				numLines < 0, 
+				"the number of input lines has been set to zero"
+			);
+		} else {
+			
+		}
+		count++;
+	}
 
 }
 
@@ -26,4 +59,19 @@ bool Scale::freqFromMIDI(int midiNote) {
 
 bool Scale::freqFromASCII(char* key) {
 
+}
+
+void scalaAssert(bool assertion, std::string message) {
+	if(assertion) return;
+
+	std::string fullMessage =
+		"\n"
+		"Unfortunately, we were unable to parse your scala file because "
+		+ message + "\n\n"
+		"Check http://www.huygens-fokker.org/scala/scl_format.html "
+		"to make sure your file was formatted correctly.\n"
+		"Otherwise, feel free to post an issue at "
+		"https://github.com/AlloSphere-Research-Group/allolib\n";
+	
+	std::cerr << fullMessage << std::endl;
 }
