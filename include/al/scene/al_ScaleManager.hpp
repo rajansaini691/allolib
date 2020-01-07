e007540e73cda88f13ea7c7885934806d37b7985
Rajan <rajansaini691@gmail.com>
2020-01-06 18:03:43 -0800

    Update docs

--- include/al/scene/al_ScaleManager.hpp
+++ include/al/scene/al_ScaleManager.hpp
@@ -16,22 +16,28 @@ class Scale {
  public:
 
   /**
-   * @brief Constructor using scala files (use for custom tunings)
+   * @brief		Constructor using scala files (use for custom tunings)
+   * @detailed		Creates a custom-tuned scale based on the given .scl
+   *			file and a given fundamental frequency
+   *			For example, to use Just Intonation based on A440, 
+   *			set the pitch standard to 440 Hz
    *
-   * @param sclPath	Path to the scala file
-   * @param A		Frequency value of A in Hz (Generally 440)
+   * @param sclPath		Path to the scala file
+   * @param tonic	Frequency value of tonic note in Hz 
    */
-  Scale(std::string sclPath, unsigned int A = 440);
+  Scale(std::string sclPath, unsigned int tonic = 440);
 
   /**
-   * @brief Constructs a scale using equal temperament (the usual tuning system)
+   * @brief		Constructs a scale using equal temperament (the usual tuning system)
+   * @detailed		If you do not need precise control over your tunings,
+   *			use this constructor instead. 
    *
-   * @param A		Frequency value of A in Hz
+   * @param tonic	Frequency value of A in Hz
    */
-  Scale(unsigned int A = 440);
+  Scale(unsigned int tonic = 440);
 
   /**
-   * @brief Provides a frequency for the given MIDI note value
+   * @brief		Provides a frequency for the given MIDI note value
    *
    * @param midiNote	An integer corresponding to a note on a piano keyboard
 			(See https://newt.phys.unsw.edu.au/jw/notes.html)
@@ -39,13 +45,26 @@ class Scale {
   bool freqFromMIDI(int midiNote);
 
   /**
-   * @brief Maps a note on a computer keyboard to a frequency value
+   * @brief		Maps a note on a computer keyboard to a frequency value
    *
    * @param key		Character of the key being pressed
    */
   bool freqFromASCII(char* key);
 
   ~Scale();
+
+  private:
+   /**
+    * Value of A in Hz (generally 440)
+    */
+   unsigned int tonic;
+
+   /**
+    * Though MIDI notes will only generally range from 21 to 108, the LUT 
+    * (lookup table) is of size 108 rather than 87 for readability
+    */
+   float tunings[108];
+
 }
 
 }
